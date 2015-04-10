cfd1ca40b230c3c5e9ce59b60d57951b6949352b
syamfikry <syamfikry@gmail.com>
2015-04-10 11:15:23 +0700

    Update JurnalNo3.java

--- Jurnal no.3/src/jurnal/no/pkg3/JurnalNo3.java
+++ Jurnal no.3/src/jurnal/no/pkg3/JurnalNo3.java
@@ -16,7 +16,12 @@ public class JurnalNo3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+         int n=5;
         
+       while(n>0){
+           n=n*n-1;
+       }
+        System.out.println(n);
     }
     
 }
